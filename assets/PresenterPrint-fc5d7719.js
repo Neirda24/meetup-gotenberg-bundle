d1a872ec9ff6c7b7afe3c757bd5e60950468bd17
Adrien Roches <adrien@roc-it.tech>
2024-05-13 16:30:42 +0200

    Update

--- assets/PresenterPrint-fc5d7719.js
+++ assets/PresenterPrint-fc5d7719.js
@@ -1,4 +1,4 @@
-import{d,u as _,a as u,c as m,b as p,r as h,o as a,e as n,f as t,t as s,g as l,F as f,h as g,n as v,i as b,j as x,k as y,l as k,m as N,_ as P}from"./index-eafbdfce.js";import{N as w}from"./NoteDisplay-f1cdb6c9.js";const T={class:"m-4"},V={class:"mb-10"},L={class:"text-4xl font-bold mt-2"},S={class:"opacity-50"},j={class:"text-lg"},B={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=d({__name:"PresenterPrint",setup(F){_(`
+import{d,u as _,a as u,c as m,b as p,r as h,o as a,e as n,f as t,t as s,g as l,F as f,h as g,n as v,i as b,j as x,k as y,l as k,m as N,_ as P}from"./index-bf7c054f.js";import{N as w}from"./NoteDisplay-727456be.js";const T={class:"m-4"},V={class:"mb-10"},L={class:"text-4xl font-bold mt-2"},S={class:"opacity-50"},j={class:"text-lg"},B={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},C=d({__name:"PresenterPrint",setup(F){_(`
 @page {
   size: A4;
   margin-top: 1.5cm;
